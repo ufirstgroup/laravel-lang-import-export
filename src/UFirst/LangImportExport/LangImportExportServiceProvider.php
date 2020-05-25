fab5153392509d226d1aae4f9250a90f621e2e8c
Sandor Lengyel <slengyel@Sandors-MacBook-Pro.local>
2020-05-25 15:26:14 +0300

    uprade

--- src/UFirst/LangImportExport/LangImportExportServiceProvider.php
+++ src/UFirst/LangImportExport/LangImportExportServiceProvider.php
@@ -6,7 +6,7 @@ use Illuminate\Support\ServiceProvider;
 
 use UFirst\LangImportExport\Console\ExportToCsvCommand;
 use UFirst\LangImportExport\Console\ImportFromCsvCommand;
-
+use UFirst\LangImportExport\LangListService;
 class LangImportExportServiceProvider extends ServiceProvider {
 
 	/**
@@ -14,7 +14,7 @@ class LangImportExportServiceProvider extends ServiceProvider {
 	 *
 	 * @var bool
 	 */
-	protected $defer = true;
+	protected $defer = false;
 
 	/**
 	 * Register the service provider.
@@ -23,9 +23,15 @@ class LangImportExportServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-		$this->registerExportToCsvCommand();
-		$this->registerImportFromCsvCommand();
-		require __DIR__.'/../../bindings.php';
+		if ($this->app->runningInConsole()) {
+			$this->commands([
+				ExportToCsvCommand::class,
+				ImportFromCsvCommand::class
+			]);
+			$this->app->singleton(LangListService::class, function ($app) {
+				return new LangListService();
+			});
+		}
 	}
 
 	/**
@@ -38,24 +44,6 @@ class LangImportExportServiceProvider extends ServiceProvider {
 		return [
 			'lang-export.csv', 'lang-import.csv'
 		];
-	}
-
-	private function registerExportToCsvCommand() {
-		$this->app['lang-export.csv'] = $this->app->share(function($app)
-		{
-			return new ExportToCsvCommand();
-		});
-
-		$this->commands('lang-export.csv');
-	}
-
-	private function registerImportFromCsvCommand() {
-		$this->app['lang-import.csv'] = $this->app->share(function($app)
-		{
-			return new ImportFromCsvCommand();
-		});
-
-		$this->commands('lang-import.csv');
 	}
 
 }
